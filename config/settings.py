95ffc120edc8410db390eefbd8c142835f65e8ad
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-09 20:47:38 -0300

    build: REST Framework

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django Rest Framework Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
