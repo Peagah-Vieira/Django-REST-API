534d868c3c850b91dd34f09fb3654ff4c2844aba
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-11 13:31:27 -0300

    feat: accounts endpoint

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import SimpleRouter
+
+from accounts import views
+
+app_name = "accounts"
+
+accounts_api_router = SimpleRouter(trailing_slash=True)
+accounts_api_router.register(
+    prefix='api',
+    viewset=views.AccountAPIViewSet,
+    basename='accounts-api',
+)
+
+urlpatterns = accounts_api_router.urls
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .serializers import AccountSerializer
+
+
+class AccountAPIViewSet(ReadOnlyModelViewSet):
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        User = get_user_model()
+        queryset = User.objects.filter(username=self.request.user.username)
+        return queryset
