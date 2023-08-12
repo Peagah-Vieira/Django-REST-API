dc63419d94f88d94e677b5483c2154f543be98a6
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-12 19:58:34 -0300

    feat: articles ModelViewSet

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,16 @@ app_name = "articles"
 
 articles_api_router = SimpleRouter(trailing_slash=True)
 
+articles_api_router.register(
+    prefix="api",
+    viewset=views.ArticlesAPIViewSet,
+    basename="articles-api",
+)
+
+articles_api_router.register(
+    prefix="categories/api",
+    viewset=views.CategoriesAPIViewSet,
+    basename="articles-category-api",
+)
+
 urlpatterns = articles_api_router.urls
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Article, Category
+from .serializers import ArticleSerializer, CategorySerializer
+
+
+class CategoriesAPIViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ArticlesAPIViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
